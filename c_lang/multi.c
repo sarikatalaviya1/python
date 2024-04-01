3705e3772adf8c94d9bd61fe8250958d34bf5718
saikatalaviya1 <rwa1.sarika.sd@gmail.com>
2024-04-01 10:56:05 +0530

    all file upload

---
+++ c_lang/multi.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ main()
+{
+
+    int no=5,ans,i;
+    for(i=0;i<=10;i++){
+        ans=no*i;
+        printf("%d * %d = %d",no,i,ans);
+    }
+
+
+}
---
+++ c_lang/sum.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+ main(int argc, char const *argv[])
+{
+
+    int a=10,b=5;
+
+    printf(" Sum is:%d",a+b);
+}
